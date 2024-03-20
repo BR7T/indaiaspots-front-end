5f11ad82001bf3da06229a9f64afd62376afc9e8
BR7T <buratinivictor@gmail.com>
2024-03-19 22:49:35 -0300

    Change the Login Design

--- src/components/LoginPage/login.jsx
+++ src/components/LoginPage/login.jsx
@@ -1,43 +1,48 @@
 import { Link , Outlet } from "react-router-dom"
-import FormLogin from "./components/FormLogin"
+import FormLogin from "./components/Form/FormLogin"
 
-import { IoLogoGoogle } from "react-icons/io";
+import { IoLogoGoogle  } from "react-icons/io";
+
 
 
 import "./login.sass"
 
 import Images from "../../../public/Image/images";
+import FooterLogin from "./components/Footer/footerLogin";
+import Header from "../HomePage/components/Header/header";
 
 export default function Login(){
 
    
 
     return(
-        <div id="LoginPage">
-        <main>
-            
+        <>
+            <div id="LoginPage">
+                <Header/>
 
-            <div id="LoginArea">
-                <div id="icon">
-                    <img src={Images.Logo} alt="Logo" />
-                    <p>IndaiaSpots</p>
-                </div>
-                <div id="title">  
-                    <h1>Login</h1>
-                </div>
-                
-                <FormLogin/>
-                <p>Ou</p>
-                <div id="alterLogin">
-                    <button><IoLogoGoogle/> Continuar com o Google</button>
-                </div>
+                <main id="mainLogin">
+                    
+                    <div id="LoginArea">
+                        
+                        <div id="title">  
+                            <h1>Login</h1>
+                        </div>
+                        
+                        <FormLogin/>
+                        <p>Ou</p>
+                        <div id="alterLogin">
+                            <button><IoLogoGoogle/> Continuar com o Google</button>
+                        </div>
+                    </div>
+                    
+
+                </main>
+                <FooterLogin/>
             </div>
-            
 
-            <aside>
-               
+            <aside id="TextAside">
+                
             </aside>
-        </main>
-        </div>
+        </>
     )
 }
