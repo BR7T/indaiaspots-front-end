9cfcd952443c0872d06abc0009096668b4d74ae1
Amarok258cv <guilherme35427@fiec.edu.br>
2024-03-28 22:39:29 -0300

    Pagina de registro

--- src/Pages/RegisterPage/Register.jsx
+++ src/Pages/RegisterPage/Register.jsx
@@ -1,9 +1,18 @@
 import React, { useState } from 'react';
 import './Register.sass';
+import { useNavigate} from 'react-router-dom';
+
+
+import { BiUser } from "react-icons/bi";
+import { MdOutlineEmail } from "react-icons/md";
+import { MdPassword } from "react-icons/md";
+import TextAside from '../LoginPage/components/TextAside/TextAside';
+
 
 
 
 const RegisterPage = () => {
+  const navigate = useNavigate()
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -26,51 +35,63 @@ const RegisterPage = () => {
     <div className="register-page">
       
       <form onSubmit={handleSubmit}>
-        <h1>Registre-se</h1>
-        <label htmlFor="name">Nome</label>
+        <h1 id='titleForm'>Registre-se</h1>
+        <label htmlFor="name">Nome <BiUser className="inputIcons"/></label>
         <input
           type="text"
           id="name"
           name="name"
+          placeholder='Insira seu nome'
           value={formData.name}
           onChange={handleChange}
           required
         />
 
-        <label htmlFor="email">Email</label>
+        <label htmlFor="email">Email <MdOutlineEmail className="inputIcons"/></label>
         <input
           type="email"
           id="email"
           name="email"
+          placeholder='Insira seu Email'
           value={formData.email}
           onChange={handleChange}
           required
         />
 
-        <label htmlFor="password">Senha</label>
+        <label htmlFor="password">Senha<MdPassword className="inputIcons"/></label>
         <input
           type="password"
           id="password"
+          placeholder='Insira sua senha'
           name="password"
           value={formData.password}
           onChange={handleChange}
           required
         />
 
-        <label htmlFor="confirm-password">Confirme a Senha</label>
+        <label htmlFor="confirm-password">Confirme<MdPassword className="inputIcons"/></label>
         <input
           type="password"
           id="confirm-password"
           name="confirmPassword"
+          placeholder='Confirme a senha'
           value={formData.confirmPassword}
           onChange={handleChange}
           required
         />
 
-        <button type="submit">Confirme</button>
+      
+      <div class="containerBotao wise">
+          <button id="btnGoBack" class="cancel botao" type="button" onClick={()=>{navigate("/")}}>Voltar</button>
+          
+          <button id="submit" class="textcolor botao" type="button">
+            Avançar</button>
+        
+        </div>
+      
       </form>
     
-    
+      <TextAside/>
     </div>
   );
  
