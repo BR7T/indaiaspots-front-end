70f1eef347190d66686cb7c285dab289ff241bec
unknown <35443@fiecalunos.lan>
2024-05-23 19:05:37 -0300

    register responsive

--- src/Pages/RegisterPage/Register.jsx
+++ src/Pages/RegisterPage/Register.jsx
@@ -1,8 +1,7 @@
-import { Link , useNavigate, Outlet, json } from "react-router-dom"
-import { IoLogoGoogle  } from "react-icons/io";
+import { Link, useNavigate, Outlet, json } from "react-router-dom";
+import { IoLogoGoogle } from "react-icons/io";
 
-
-import "./Register.sass"
+import "./Register.sass";
 
 import Images from "../../../public/Image/images";
 import Header from "../HomePage/components/Header/header";
@@ -14,49 +13,43 @@ import FormRegister from "./components/FormRegister/FormRegister";
 import { IoMdArrowRoundBack } from "react-icons/io";
 import BackButton from "../../standard/LoginComponents/BackButton";
 
+export default function Register() {
+  const Navigate = useNavigate();
 
+  return (
+    <>
+      <div id="RegisterPage" className="">
+        <aside id="TextAsideArea" className="hidden xl:block">
+          <TextAside />
+        </aside>
+        <main
+          id="mainRegister"
+          className="p-10 xl:p-0 xl:w-2/6 h-2/3 rounded-xl flex justify-center items-center"
+        >
+          <div id="RegisterArea" className=" justify-center h-full">
+            <BackButton pagelink="/" />
 
-
-export default function Register(){
-
-   const Navigate = useNavigate()
-
-    return(
-        <>
-                
-            <div id="RegisterPage" className="">
-            <aside id="TextAsideArea">
-                    <TextAside/>
-                </aside>
-                <main id="mainRegister" className="w-2/5 h-5/6 rounded-xl  ">
-                    
-                    <div id="RegisterArea" className=" justify-center h-full" >
-                        <BackButton pagelink='/'/>
-                        
-                        <div id="title">  
-                            <h1 className="text-5xl text-start">Cadastre-se</h1>
-                        </div>
-                        
-                        <FormRegister/>
-                        <p>Ou</p>
-                        <div id="alterRegister" onClick={()=>{
-                            signInWithGoogle();
-                        }}>
-                            <button
-                            ><IoLogoGoogle/> Continuar com o Google</button>
-                        </div>
-                        <div className=" -right-1/2 absolute bg-red-600 w-1/2 h-1/4 rounded-3xl bg-white shadow bottom-0">
-
-                        </div>
-                    </div>
-
-                </main>
-               
+            <div id="title">
+              <h1 className="text-4xl md:text-5xl text-start">Cadastre-se</h1>
             </div>
-            
-            <FooterLogin/>
 
-            
-        </>
-    )
+            <FormRegister />
+            <p>Ou</p>
+            <div
+              id="alterRegister"
+              onClick={() => {
+                signInWithGoogle();
+              }}
+            >
+              <button>
+                <IoLogoGoogle /> Continuar com o Google
+              </button>
+            </div>
+          </div>
+        </main>
+      </div>
+
+      <FooterLogin />
+    </>
+  );
 }
